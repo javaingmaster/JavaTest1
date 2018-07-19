5d1cd98a6979fa59fecd534790b62b933d049d12
zty <656358805@qq.com>
2018-07-19 12:00:04 +0800

    update

--- Exam3/src/main/java/com/hand/ImplClass/ListOfThreeFactory.java
+++ Exam3/src/main/java/com/hand/ImplClass/ListOfThreeFactory.java
@@ -19,11 +19,15 @@ public class ListOfThreeFactory implements ProcessList {
 		List<Integer> sign=new ArrayList<>();
 		List<Integer> numbers=new ArrayList<>();
 		Map<Integer,List<Integer>> map = new HashMap<>();
-
+		/**
+		 * 生成随机数
+		 */
 		for(int i=0;i<50;i++){
 			numbers.add((int)(Math.random()*100));
 		}
-
+		/**
+		 * stream处理数据结构
+		 */
 		numbers.stream().forEach(integer -> {
 			int key=integer/10;
 			if(map.get(key)==null){
@@ -34,12 +38,18 @@ public class ListOfThreeFactory implements ProcessList {
 			}
 		});
 
+		/**
+		 * 迭代排序
+		 */
 		Collection<List<Integer>> list =map.values();
 		Iterator<List<Integer>> integerIterator = list.iterator();
 		while(integerIterator.hasNext()){
 			ProcessImpl.getInstance().process(integerIterator.next());
 		}
 
+		/**
+		 * 输出排序
+		 */
 		for(int i=0;i<sign.size();i++){
 			System.out.println(map.get(sign.get(i)).toString());
 		}
